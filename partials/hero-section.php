0d382dfc0ef83700cd6b60ceb98c03862aac4b7e
iamkire <kiree.ruzinski@gmail.com>
2022-07-26 01:47:22 +0200

    small css change

--- partials/hero-section.php
+++ partials/hero-section.php
@@ -1,4 +1,4 @@
-<section class="bg-white dark:bg-gray-900">
+<section class="bg-white dark:bg-gray-900 pt-36 ">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to my mini clinic</h1>
         <form action="Auth/login.php" method="post">
